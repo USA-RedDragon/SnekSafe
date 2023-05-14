8a0bbbd8691706caaed5e9d590421d377ce3477a
Jacob McSwain <jacob@mcswain.dev>
2023-05-14 18:42:22 -0500

    frontend: Add setup route

--- frontend/src/views/SetupView.vue
+++ frontend/src/views/SetupView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="setup">
+    <h1>This is a setup page</h1>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .setup {
     min-height: 100vh;
     display: flex;
     align-items: center;
