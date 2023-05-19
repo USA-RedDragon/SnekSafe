5dd3773ba42d339fee92fe68fe3be2b31efe6f18
Jacob McSwain <jacob@mcswain.dev>
2023-05-19 05:08:50 -0500

    frontend: api: Use mDNS for local testing

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
+let baseURL;
+
+// nodejs development
+if (window.location.port == 5173) {
+  // Use mDNS address
+  baseURL = 'http://snek.local/api/v1';
+} else {
+  baseURL = '/api/v1';
+}
+
 const instance = axios.create({
-  baseURL: '/api/v1',
+  baseURL,
 });
 
 export default instance;
