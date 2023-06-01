de774d9dd967113cc673723c1c6aa6352ff4aaca
Jacob McSwain <jacob@mcswain.dev>
2023-05-31 22:22:22 -0500

    ci: add firmware releaser

---
+++ firmware/scripts/git_version_macro.py
@@ -0,0 +1,12 @@
+import subprocess
+
+revision = (
+    subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+    .strip()
+    .decode("utf-8")
+)
+
+version = open(".version", "r").read().strip()
+
+
+print("'-DGIT_COMMIT=\"%s\"' '-DVERSION=\"%s\"'" % (revision, version))
